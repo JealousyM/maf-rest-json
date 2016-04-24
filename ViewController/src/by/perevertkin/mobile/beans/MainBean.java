f09e0262c80e674cc7baa5958223c15f1ea78d87
Perevertkin Mikhail <perevertkinma@gmail.com>
2016-04-24 14:57:07 +0300

    initial

--- ViewController/src/by/perevertkin/mobile/beans/MainBean.java
+++ ViewController/src/by/perevertkin/mobile/beans/MainBean.java
@@ -4,42 +4,42 @@ import by.perevertkin.mobile.serviceresult.ServiceResult;
 
 import by.perevertkin.mobile.serviceresult.dc.ServiceResultDC;
 
+import java.util.ArrayList;
+
+import javax.el.ValueExpression;
+
 import oracle.adfmf.dc.ws.rest.RestServiceAdapter;
+import oracle.adfmf.framework.api.AdfmfJavaUtilities;
 import oracle.adfmf.framework.api.JSONBeanSerializationHelper;
 import oracle.adfmf.framework.api.MafExecutorService;
 import oracle.adfmf.framework.api.Model;
+import oracle.adfmf.framework.exception.AdfInvocationException;
 import oracle.adfmf.java.beans.PropertyChangeListener;
 import oracle.adfmf.java.beans.PropertyChangeSupport;
 
 public class MainBean {
-    private  int postId;
-    private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
 
     public MainBean() {
     }
 
     public String getPost() {
-        ServiceResultDC serviceResultDC=new ServiceResultDC();
-        serviceResultDC.getPost(getPost().toString());
+        
+        ValueExpression ve = AdfmfJavaUtilities.getValueExpression("#{pageFlowScope.postId}", String.class);
+        String postId = (String) ve.getValue(AdfmfJavaUtilities.getAdfELContext());
+        ArrayList parameterNames = new ArrayList();
+        parameterNames.add("id");
+        ArrayList parameterTypes = new ArrayList();
+        parameterTypes.add(String.class);
+        ArrayList parameterValues = new ArrayList();
+        parameterValues.add(postId);
+        try {
+            AdfmfJavaUtilities.invokeDataControlMethod("ServiceResultDC", null, "getPost", parameterNames,
+                                                       parameterValues, parameterTypes);
+
+        } catch (AdfInvocationException e) {
+            System.out.println("Error in action 'getPost':"+e); 
+        }
         return "details";
     }
 
-    public void setPostId(int postId) {
-        int oldPostId = this.postId;
-        this.postId = postId;
-        propertyChangeSupport.firePropertyChange("postId", oldPostId, postId);
-    }
-
-    public int getPostId() {
-        return postId;
-    }
-
-
-    public void addPropertyChangeListener(PropertyChangeListener l) {
-        propertyChangeSupport.addPropertyChangeListener(l);
-    }
-
-    public void removePropertyChangeListener(PropertyChangeListener l) {
-        propertyChangeSupport.removePropertyChangeListener(l);
-    }
 }
